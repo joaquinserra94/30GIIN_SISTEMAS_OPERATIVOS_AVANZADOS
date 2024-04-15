add7ff85ba40773ae269df2b6da5c0e03b23bbfe
Joaquin <joaquinserra94@gmail.com>
2024-04-15 18:56:52 +0200

    Actividad2 de UC1 13/04

---
+++ Copia de un fichero.dSYM/clase15.4.c
@@ -0,0 +1,21 @@
+#include <sys/types.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h>
+int main(int argc, char** argv) { 
+pid_t pid;
+pid = fork();
+switch (pid) {
+case -1: /* error */
+exit(-1);
+case 0: /* proceso hijo */
+if (execvp(argv[1], &argv[1]
+)<0) {
+perror("error");
+}
+break; 
+default:
+printf("Proceso padre");
+}
+return 0;
+}
